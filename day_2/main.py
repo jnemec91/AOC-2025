8e8add26721ae1d9def32ae26616a03715dc5fc7
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-12-02 06:29:27 +0100

    Day 2, Part 1

--- day_2/main.py
+++ day_2/main.py
@@ -1,8 +1,25 @@
 import os
 
+def solve(ranges: list[str]) -> int:
+    """
+    Finds invalid IDs in input.
+    """
+    # half of number - other halg should be zero
+    result = 0
+    for r in ranges:
+        r = r.split('-')
+        for i in range(int(r[0]), int(r[1])+1):
+            if len(str(i))%2 == 0:
+                middle = int(len(f'{i}')/2)
+                if int(f'{i}'[0:middle]) - int(f'{i}'[middle::]) == 0:
+                    result += i
+
+    return result
 
 if __name__ == '__main__':
     with open(
-    os.path.join(os.path.dirname(__file__), 'inputs', 'test_input.txt'), 'r', encoding='UTF-8'
+    os.path.join(os.path.dirname(__file__), 'inputs', 'input.txt'), 'r', encoding='UTF-8'
     ) as file:
-        data = file.read()
+        data = file.read().split(',')
+
+    print(solve(data))
