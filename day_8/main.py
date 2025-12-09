376efdbdaba812b14ecf9c273ad2b80ccda2edb3
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-12-09 06:45:55 +0100

    Day 8, Part 2

--- day_8/main.py
+++ day_8/main.py
@@ -1,150 +1,118 @@
+#pylint:disable=W0621
 """
-Solves light connection of junction boxes
+Solves light connection of junction boxes. 
 """
 
 import os
 import math
 import time
-from typing import Self
 
-class Point:
+class DSU:
     """
-    Represents point in 3D space.
+    Disjoint Set Union.
+    https://www.geeksforgeeks.org/dsa/introduction-to-disjoint-set-data-structure-or-union-find-algorithm/
     """
-    def __init__(self, x,y,z):
-        self.x : int = x
-        self.y : int = y
-        self.z : int = z
-        self.distances : dict[float, Point] = {}
-        self.in_circuit : Circuit = None
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        self.circuits = n
 
-    def __str__(self) -> str:
-        return f'Point{self.x, self.y, self.z}'
+    def find(self, x):
+        """Find root of x with path compression."""
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
-    def __repr__(self) -> str:
-        return f'Point{self.x, self.y, self.z}'
+    def union(self, x, y):
+        """Union two sets by rank."""
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
 
-    def calculate_distance(self, point) -> Self:
-        """
-        Calculates distance to another point using euclidian distance formula.
-        """
-        p1, p2, p3 = point.x, point.y, point.z
-        q1, q2, q3 = self.x, self.y, self.z
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+            self.circuits -= 1
+            return True
+        return False
 
-        distance = round(math.sqrt((p1 - q1)**2 + (p2 - q2)**2 + (p3 - q3)**2), 3)
+    def get_circuits_count(self):
+        """Get number of connected circuits."""
+        return self.circuits
 
-        self.distances[distance] = point
+    def get_circuit_sizes(self):
+        """Get sizes of all circuits."""
+        circuit_size = {}
+        for i in range(len(self.parent)):
+            root = self.find(i)
+            circuit_size[root] = circuit_size.get(root, 0) + 1
+        return list(circuit_size.values())
 
-        return self
 
-    def get_closest(self) -> int | None:
-        """
-        Returns shortest distance to neighbour or none if nothing is in distances dict.
-        """
-        keys : list[float] = [i for i in self.distances]
-
-        if len(keys) == 0:
-            return None
-
-        return min(keys)
-
-class Circuit:
+def solve(points : list[list[int]],
+          part_two : bool = True,
+          iterations : int = 0) -> tuple[list[int], tuple[int, int]]:
     """
-    Represents set of points. 
-    """
-    def __init__(self) -> None:
-        self.points : list[Point] = []
-
-    def __repr__(self) -> str:
-        return f'Circuit_l_{len(self.points)}{[str(i) for i in self.points]}\n'
-
-def solve(points : list[list[int]]) -> list[Circuit]:
-    """
-    Connects circuits.
+    Solves connection of junction boxes using DSU.
     """
     start : time.time = time.time()
-    circuits : list[Circuit] =  []
+    n = len(points)
+    dsu = DSU(n)
+    last_connected = None
 
-    for i, point in enumerate(points):
-        points[i] = Point(*point)
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            p1, p2 = points[i], points[j]
+            distance = math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+            edges.append((distance, i, j))
 
-    for point in points:
-        for other_point in points:
-            if point != other_point:
-                point.calculate_distance(other_point)
+    edges.sort()
 
+    iteration = 0
+    for _, i, j in edges:
+        iteration += 1
+        if dsu.union(i, j):
+            last_connected = (i, j)
+            circuits = dsu.get_circuits_count()
 
-    for iteration in range(0, 1000):
-        if math.floor(iteration/10)%1 == 0:
-            os.system('cls')
-            print(f'Progress: { math.floor(iteration/10) * '■' }{(100-math.floor(iteration/10)) *' '} {math.floor(iteration/10)}% running: {round((time.time() - start), 2)}s') #pylint:disable=C0301
+            if circuits == 1:
+                break
 
-        smallest_distance : float | None = None
-        smallest_distance_points : tuple[Point] | None = None    
+            if not part_two:
+                if iteration == iterations:
+                    break
 
-        for point in points:
-            point_smallest = point.get_closest()
-            if point_smallest is None:
-                continue
+    bcs = dsu.get_circuit_sizes()
+    bcs.sort(reverse=True)
 
-            if smallest_distance is None:
-                smallest_distance = point.get_closest()
-                smallest_distance_points = point, point.distances[point.get_closest()]
+    print(f'finshed in {round(time.time() - start, 2)}s')
+    return bcs, last_connected
 
-            if  point_smallest < smallest_distance:
-                smallest_distance = point.get_closest()
-                smallest_distance_points = point, point.distances[point.get_closest()]
-
-
-        if smallest_distance_points[0].in_circuit is None and smallest_distance_points[1].in_circuit is None: #pylint:disable=C0301
-            c = Circuit()
-            for i in smallest_distance_points:
-                c.points.append(i)
-                i.in_circuit = c
-            circuits.append(c)
-
-        elif smallest_distance_points[0].in_circuit is not None and smallest_distance_points[1].in_circuit is None: #pylint:disable=C0301
-            c = smallest_distance_points[0].in_circuit
-            c.points.append(smallest_distance_points[1])
-            smallest_distance_points[1].in_circuit = c
-
-        elif smallest_distance_points[1].in_circuit is not None and smallest_distance_points[0].in_circuit is None: #pylint:disable=C0301
-            c = smallest_distance_points[1].in_circuit
-            c.points.append(smallest_distance_points[0])
-            smallest_distance_points[0].in_circuit = c
-
-        elif smallest_distance_points[0].in_circuit is not None and smallest_distance_points[1].in_circuit is not None: #pylint:disable=C0301
-            circuit1 = smallest_distance_points[0].in_circuit
-            circuit2 = smallest_distance_points[1].in_circuit
-
-            if circuit1 != circuit2:
-                for point in circuit2.points:
-                    circuit1.points.append(point)
-                    point.in_circuit = circuit1
-                circuits.remove(circuit2)
-
-        for i in smallest_distance_points:
-            i.distances.pop(smallest_distance)
-
-    for point in points:
-        if point.in_circuit is None:
-            c = Circuit()
-            c.points.append(point)
-            circuits.append(c)
-
-    return circuits
 
 if __name__ == '__main__':
     os.system('cls')
-    for filename in ['input']:
+    for filename in ['test_input', 'input']:
+        print(f'Solving {filename}.txt')
         with open(
         os.path.join(os.path.dirname(__file__), 'inputs', f'{filename}.txt'), 'r', encoding='UTF-8'
         ) as file:
             data = file.read().splitlines()
             data = [[int(a) for a in i.split(',')] for i in data]
+            for part in [False,True]:
+                print(f'Solving {"part one" if not part else "part two"}.')
+                if not part:
+                    ITERATIONS = 10 if filename == 'test_input' else 1000
+                    circuit_sizes, last_connected = solve(data, part, ITERATIONS)
 
-            cc = solve(data)
-            cc.sort(key=lambda x: len(x.points), reverse=True)
+                    result = circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
+                    print(f'Biggest three circuits multiplied after {ITERATIONS} = {result}.\n')
+                else:
+                    circuit_sizes, last_connected = solve(data)
 
-            total = len(cc[0].points) * len(cc[1].points) * len(cc[2].points)
-            print(f'Result for part 1 is: {total}')
+                    if last_connected:
+                        i, j = last_connected
+                        print(f'Last two points x values multiplied = {data[i][0]*data[j][0]}.\n')
